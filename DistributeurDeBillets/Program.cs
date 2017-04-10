691bcbbb4fb91ebeffd57139d778601426e51c38
Alexandre Liscia <alexandre.liscia@gmail.com>
2017-04-10 09:57:14 +0200

    Ajout du projet de distributeur de billets

---
+++ DistributeurDeBillets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DistributeurDeBillets
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, veuillez insérer votre carte");
+
+            // Attendre que l'utilisateur appuye sur la touche entrée
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+
+            }
+
+            Console.WriteLine("Merci de saisir votre code secret.");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Quel opération souhaitez-vous effectuer ?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Quel montant désirez-vous retirer ?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Interrogation du compte en cours...");
+
+            System.Threading.Thread.Sleep(5000);
+
+            Console.WriteLine("Pour obtenir vos billets, merci de récupérer votre carte.");
+        }
+    }
+}
