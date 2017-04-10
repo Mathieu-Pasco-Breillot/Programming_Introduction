eef9e3f2c86123b0c1711daf2888ca88bffae1e5
Mathieu PASCO-BREILLOT <mathieu.pasco@gmail.com>
2017-04-10 11:03:54 +0200

    Renommage du projet. Ajout de vérification pour le code secret.

--- DistributeurDeBillets/Program.cs
+++ DistributeurDeBillets/Program.cs
@@ -9,15 +9,29 @@ namespace DistributeurDeBillets
         {
             Console.WriteLine("Bonjour, veuillez insérer votre carte");
 
-            // Attendre que l'utilisateur appuye sur la touche entrée
-            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            WaitForEnterKey();
+
+            Console.WriteLine("Merci de saisir votre code secret à 4 chiffres.");
+
+            var codeSecret = Console.ReadLine();
+            codeSecret = CheckLength(codeSecret);
+
+            bool conversionPassed = false;
+
+            while (!conversionPassed)
             {
-
+                try
+                {
+                    var code = Convert.ToInt16(codeSecret);
+                    conversionPassed = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Merci de saisir uniquement des chiffres.");
+                    codeSecret = Console.ReadLine();
+                    codeSecret = CheckLength(codeSecret);
+                }
             }
-
-            Console.WriteLine("Merci de saisir votre code secret.");
-
-            Console.ReadLine();
 
             Console.WriteLine("Quel opération souhaitez-vous effectuer ?");
 
@@ -32,6 +46,30 @@ namespace DistributeurDeBillets
             System.Threading.Thread.Sleep(5000);
 
             Console.WriteLine("Pour obtenir vos billets, merci de récupérer votre carte.");
+
+            WaitForEnterKey();
+        }
+
+        private static string CheckLength(string codeSecret)
+        {
+            while (codeSecret.Length != 4)
+            {
+                Console.WriteLine("Merci de saisir 4 chiffres.");
+                codeSecret = Console.ReadLine();
+            }
+
+            return codeSecret;
+        }
+
+        /// <summary>
+        /// Permet d'attendre que l'utilisateur appuie sur la touche entrée.
+        /// </summary>
+        private static void WaitForEnterKey()
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+
+            }
         }
     }
 }
